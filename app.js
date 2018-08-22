88274bf3a92fab0632efffc0f03eb69cb1e567a6
Zsolt Meszaros <zsolt@sowad.com>
2018-08-22 22:29:11 +0200

    Add logger middleware

--- app.js
+++ app.js
@@ -2,9 +2,13 @@
 
 const express = require('express');
 const path = require('path');
+
+const logger = require('./middlewares/logger');
 const routes = require('./routes');
 
 const app = express();
+
+app.use(logger);
 
 app.set('port', process.env.PORT || 8000);
 app.set('views', path.join(__dirname, 'views'));
