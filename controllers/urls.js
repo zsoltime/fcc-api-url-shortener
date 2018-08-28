1e8447e1848cb3d489da8f852f0a2f2d67f0d4fd
Zsolt Meszaros <zsolt@sowad.com>
2018-08-28 13:41:29 +0100

    Add DNS lookup

--- controllers/urls.js
+++ controllers/urls.js
@@ -1,21 +1,33 @@
 'use strict';
 
+const dns = require('dns');
 const fullUrl = require('requested-url');
+const url = require('url');
+
 const Url = require('../models/Url');
 
 module.exports.create = (req, res, next) => {
   // TODO: Get the API path from somewhere else. It's still too hacky
   const pathToApi = fullUrl(req).replace(/new\/?$/, '');
+  const { hostname } = url.parse(req.body.url);
 
-  Url.create({
-    url: req.body.url,
-  }).then(
-    savedUrl => res.json({
-      url: savedUrl.url,
-      short_url: `${pathToApi}${savedUrl.id}`,
-    }),
-    err => next(err)
-  );
+  dns.lookup(hostname, (lookupError) => {
+    if (lookupError) {
+      const error = new Error('Invalid hostname');
+      error.status = 400;
+      return next(error);
+    }
+
+    Url.create({
+      url: req.body.url,
+    }).then(
+      savedUrl => res.json({
+        url: savedUrl.url,
+        short_url: `${pathToApi}${savedUrl.id}`,
+      }),
+      err => next(err)
+    );
+  });
 };
 
 module.exports.get = (req, res, next) => {
