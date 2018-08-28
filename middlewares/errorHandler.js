531c22113cb8a6bf210a642ff86e650af910be4c
Zsolt Meszaros <zsolt@sowad.com>
2018-08-28 13:25:02 +0100

    Add better validation error message

--- middlewares/errorHandler.js
+++ middlewares/errorHandler.js
@@ -5,7 +5,10 @@ const validation = require('express-validation');
 // eslint-disable-next-line no-unused-vars
 function errorHandler(err, req, res, next) {
   if (err instanceof validation.ValidationError) {
-    return res.status(err.status).json(err);
+    return res.status(err.status).json({
+      status: err.status,
+      message: err.errors.map(e => e.messages[0]).join('. '),
+    });
   }
 
   return res.status(err.status || 500).json({
--- validations/urls.js
+++ validations/urls.js
@@ -7,6 +7,14 @@ module.exports.addUrl = {
     url: Joi.string()
       .uri({ allowQuerySquareBrackets: true })
       .trim()
-      .required(),
+      .required()
+      .error(errors => ({
+        template: 'contains {{amount}} {{word}}: {{codes}}',
+        context: {
+          codes: errors.map(err => err.toString()).join(', '),
+          amount: errors.length,
+          word: errors.length === 1 ? 'error' : 'errors',
+        },
+      })),
   },
 };
